e168a2b27c0691694b38843580d8646109abe884
Keya-Nagar25 <keyanagar20@gmail.com>
2026-05-11 23:37:47 +0530

    -

--- server/models/PendingOTP.js
+++ server/models/PendingOTP.js
@@ -1,7 +1,3 @@
-// models/PendingOTP.js
-// Temporary storage for OTPs during new user/driver registration
-// Auto-deletes after 10 minutes
-
 const mongoose = require('mongoose');
 
 const pendingOTPSchema = new mongoose.Schema(
@@ -23,7 +19,7 @@ const pendingOTPSchema = new mongoose.Schema(
     expiresAt: {
       type: Date,
       required: true,
-      index: { expires: 0 }, // Auto-delete after this timestamp
+      index: { expires: 0 }, 
     },
   },
   { timestamps: true }
